6828801e2d31ad4b31a2857492b1026bc38b3347
spaquet <stephan.paquet@duproprio.com>
2016-05-05 14:57:26 -0400

    Add tutorial #7 + #8

--- 2-react-router/src/js/pages/Layout.js
+++ 2-react-router/src/js/pages/Layout.js
@@ -1,36 +1,17 @@
 import React from 'react';
-import {Link} from "react-router";
+
+import Nav from "../components/layout/Nav";
+import Footer from "../components/layout/Footer";
 
 export default class Layout extends React.Component {
     render() {
+       const { location } = this.props;
         return (
             <div>
-                <nav class="navbar navbar-default">
-                  <div class="container-fluid">
-                    <div class="navbar-header">
-                      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                      </button>
-                      <a class="navbar-brand" href="#">Home</a>
-                    </div>
-
-                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                      <ul class="nav navbar-nav">
-                        <li>
-                            <Link to="archives">Archives</Link>
-                        </li>
-                        <li>
-                            <Link to="settings">Settings</Link>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </nav>
+                <Nav location={location}></Nav>
                 <h1>KillerNews.net</h1>
                 {this.props.children}
+                <Footer></Footer>
             </div>
         )
     }
